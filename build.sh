ce27f8076bbd3a83d6032e6455b8672e4d0a4b45
Luxferre <3335173-suborg@users.noreply.gitlab.com>
2019-01-10 17:56:47 +0200

    Fixed the symlinks in the build script

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+echo 'Populating symlinks...'
+rm -rf ./src/root/system/bin
+mkdir -p ./src/root/system/bin
+for util in $(<bbutils.txt); do 
+  ln -s /sbin/busybox ./src/root/system/bin/$util
+done
+chmod 0777 ./src/root/system/bin/*
+echo 'Packing the image...'
 rm -rf ./tools/Android_boot_image_editor/build/unzip_boot ./recovery.new.img
 mkdir -p ./tools/Android_boot_image_editor/build/unzip_boot
 cp -R ./src/* ./tools/Android_boot_image_editor/build/unzip_boot/
